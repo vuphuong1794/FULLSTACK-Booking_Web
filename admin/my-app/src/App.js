19decced0415aa3b3087814d008e37c0f4445d97
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-25 20:26:31 +0700

    add admin login UI and ProtectedRoute for admin page

--- admin/my-app/src/App.js
+++ admin/my-app/src/App.js
@@ -3,26 +3,41 @@ import Login from "./pages/login/Login.jsx";
 import New from "./pages/new/New.jsx";
 import List from "./pages/list/List.jsx";
 import Single from "./pages/single/Single.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss"
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext.js";
+
+
 function App() {
+  //truoc khi thuc hien in ra trang se xac minh xem nguoi dung da dang nhap hay chua
+  const ProtectedRoute = ({children})=>{
+    const {user} = useContext(AuthContext);
+
+    if(!user){
+      return <Navigate to="/login"/>
+    }
+
+    return children;
+  }
+
   return (
     <div className="app dark">
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+          <Route path="login" element={<Login />} />
+            <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+              <Route index element={<ProtectedRoute><List /></ProtectedRoute>} />
+              <Route path=":userId" element={<ProtectedRoute><Single /></ProtectedRoute>} />
+              <Route path="new" element={<ProtectedRoute><New inputs={userInputs} title="Add New User" /></ProtectedRoute>} />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>} />
+              <Route index element={<ProtectedRoute><List /></ProtectedRoute>} />
+              <Route path=":productId" element={<ProtectedRoute><Single /></ProtectedRoute>} />
+              <Route path="new" element={<ProtectedRoute><New inputs={productInputs} title="Add New Product"/></ProtectedRoute>} />
             </Route>
           </Route>
         </Routes>
