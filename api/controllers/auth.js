8de45d1df09d30bdf6ab5ba29eb4bed14936a9ff
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-26 13:12:15 +0700

    fix admin

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -10,8 +10,13 @@ export const register = async (req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const newUser = new User({
+    /*
       username: req.body.username,
       email: req.body.email,
+      */
+
+     //lay het thong tin cua nguoi dung 
+     ...req.body,
       password: hash,
     });
     await newUser.save();
@@ -26,28 +31,25 @@ export const login = async (req, res, next) => {
     const user = await User.findOne({ username: req.body.username });
     if (!user) return next(createdError(404, "User not found!"));
 
-    const checkPassword = await bcrypt.compare(
+    const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
     );
+    if (!isPasswordCorrect)
+      return next(createdError(400, "Wrong password or username!"));
 
-    if (!checkPassword)
-      return next(createdError(403, "Wrong password or username!"));
-
-    //khong hien password va thong tin admin
-    const { password, isAdmin, ...otherDetails } = user._doc;
-
-    //luu thong tin nguoi dung, su dung token base authentication va luu vao cookie de dung xac minh va phan quyen
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT,
-      { expiresIn: "30s" }
+      process.env.JWT
     );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
     res
-      //khong cho client lay cookie thong qua javascript
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
       .status(200)
-      .send({ details: {...otherDetails}, isAdmin });
+      .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
