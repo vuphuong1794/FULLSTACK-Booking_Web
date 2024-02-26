a767242d7b53aae6c420ffc2828a1279bd46f880
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-26 19:51:13 +0700

    fix unauthorized in users page

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -40,7 +40,7 @@ export const login = async (req, res, next) => {
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
       .cookie("access_token", token, {
-        httpOnly: false,
+        httpOnly: true,
       })
       .status(200)
       .json({ details: { ...otherDetails }, isAdmin });
