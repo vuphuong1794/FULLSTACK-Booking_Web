1369fc12d6958940d1dcaa7a792a58bb4989bb06
vuphuong1794 <phuongvu1794@gmail.com>
2024-03-07 09:24:34 +0700

    test url

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -22,7 +22,7 @@ const Datatable = ({columns}) => {
   //them withCredentials: true (tranh loi khong xac thuc duoc nguoi dung cua axios)
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:8800/api/${path}/${id}`,{withCredentials: true,})
+      await axios.delete(`https://mern-booking-web.onrender.com/api/${path}/${id}`,{withCredentials: true,})
       setList(list.filter((item) => item._id !== id));
     } catch(err){
       console.log(err);
