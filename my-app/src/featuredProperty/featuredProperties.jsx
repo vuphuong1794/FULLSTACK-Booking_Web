f80f728b50587d5c7b39f56474ae01eee304aebd
vuphuong1794 <phuongvu1794@gmail.com>
2024-03-28 10:23:48 +0700

    add link to featuredProperties

--- my-app/src/featuredProperty/featuredProperties.jsx
+++ my-app/src/featuredProperty/featuredProperties.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./featuredProperties.css";
 import useFetch from "../Hooks/useFetch";
+import { Link } from "react-router-dom";
 
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch(
@@ -15,20 +16,23 @@ const FeaturedProperties = () => {
         "Loading"
       ) : (
         <>
-          {limitedData && limitedData.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img src={item.photos[0]} alt="" className="fpImg" />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">
-                Starting from ${item.cheapestPrice}
-              </span>
-              <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
+          {limitedData &&
+            limitedData.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <Link to={`/hotels/${item._id}`}>
+                  <img src={item.photos[0]} alt="" className="fpImg" />
+                  <span className="fpName">{item.name}</span>
+                  <span className="fpCity">{item.city}</span>
+                  <span className="fpPrice">
+                    Starting from ${item.cheapestPrice}
+                  </span>
+                  <div className="fpRating">
+                    <button>{item.rating}</button>
+                    <span>Excellent</span>
+                  </div>
+                </Link>
               </div>
-            </div>
-          ))}
+            ))}
         </>
       )}
     </div>
